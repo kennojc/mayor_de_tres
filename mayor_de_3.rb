4a5b8bbe3cf8bd39592cb2ff9126b01b23394e0a
kennojc <kennojc@gmail.com>
2020-10-19 23:58:09 -0300

    Desafío listo.

--- mayor_de_3.rb
+++ mayor_de_3.rb
@@ -2,9 +2,9 @@ number_1 = ARGV[0].to_i
 number_2 = ARGV[1].to_i
 number_3 = ARGV[2].to_i
 
-if (number_1 > number_2) && (number_1 > number_3)
+if (number_1 >= number_2) && (number_1 >= number_3)
   puts "#{number_1}"
-elsif (number_2 > number_1) && (number_2 > number_3)
+elsif (number_2 >= number_1) && (number_2 >= number_3)
   puts "#{number_2}"
 else
   puts "#{number_3}"
